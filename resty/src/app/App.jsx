d894d626001c794e4841f2acbfd3377aef4dec25
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-06-21 21:28:54 +0300

    props and state

--- resty/src/app/App.jsx
+++ resty/src/app/App.jsx
@@ -3,18 +3,45 @@ import './App.scss';
 // import the header
 import Header from '../header/header';
 // import the main
-import Main from '../main/main';
+import Form from '../main/form';
+// import the result
+import Result from '../main/result';
 // import the footer
 import Footer from '../footer/footer';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </React.Fragment>
-  );
+// create class for app
+class App extends React.Component{
+  //constructor
+  constructor(props){
+    super(props);
+    this.state = {
+      headers:{},
+      response: {}
+    }
+  }
+  //start firing the event
+  handleForm = (headers,response)=>{
+    // console.log(response,'response......');
+    //calling the setState to update the state
+    this.setState({headers,response: response});
+    // console.log(this.state,'this.state');
+  }
+  //render method
+  render() {
+    return (
+      <React.Fragment>
+        <div className="body">
+          <Header/>
+          <Form formHandler={this.handleForm}/>
+          <Result headers={this.state.headers} response={this.state.response}/>
+          <Footer/>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
+
+
+
 
 export default App;
--- resty/src/resty.md
+++ resty/src/resty.md
@@ -1,4 +1,4 @@
-### **Component Based UI**
+## **Component Based UI**
 
 ### **LAB - 26**
 
@@ -45,11 +45,45 @@ And as developers, here are the high level development tasks that address the ab
 * **npm start**
 
 
-## **UML DIAGRAM**
+## **UML DIAGRAM PHASE-1**
 
 ![UML](1.JPG)
 
 ***
+***
+***
+
+## **Props And State**
+
+### **LAB - 27**
+
+#### **Phase 2 Requirements**
+
+In phase 2, we will be connecting RESTy to live APIs, fetching and displaying remote data. Our primary focus will be to service GET requests
+
+The following user stories detail the major functionality for this phase of the project.
+
+* As a user, I want to enter the URL to an API and issue a GET request so that I can retrieve it’s data
+* As a user, I want to see the results returned from an API request in my browser in a readable format
+
+Application Flow:
+
+* User enters an API URL
+* Chooses a REST Method
+* Clicks the “Go” button
+* Application fetches data from the URL given, with the method specified
+* Displays the response headers and results separately
+* Both headers and results should be “pretty printed” JSON
+
+
+## **UML DIAGRAM PHASE-2**
+
+![UML](2.JPG)
+
+***
+***
+***
+
 
 ***Thank you!!***
 
